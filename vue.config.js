6fc15a67543df55059f8b8743566b2d359436838
Anton Kostyuk <anton.kostuk.2012@gmail.com>
2023-06-28 17:16:19 +0300

    Add PWA name

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
+    name: 'Payouts PWA',
     prefer_related_applications: true,
     manifestOptions: {
       share_target: {
